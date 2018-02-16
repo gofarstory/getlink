a63ef041fabced5aec3675c20907eade5a70dc85
gofarstory <36549514+gofarstory@users.noreply.github.com>
2018-02-17 06:27:43 +0700

    Update postlink.js

--- postlink.js
+++ postlink.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2018/02/what-is-google-lens-and-how-do-you.html"}
 if(x=="2"){xxx="/2018/02/what-does-future-hold-for-smartphone.html"}
 if(x=="3"){xxx="/2018/02/technology-and-legal-profession-perfect.html"}
 if(x=="4"){xxx="/2018/02/surveillance-technology-now-and.html"}
-if(x=="5"){xxx="/2018/02/the-bitcoin-loud-cry.html"}a_to_ve[i].href="https://getlinksafe.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2018/02/the-bitcoin-loud-cry.html"}a_to_ve[i].href="https://getlinksafe.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
